0e2b31e40cbe70ffd53deca4bd56e2237359152c
Bebli Carl <beblicarl.cb@gmail.com>
2020-07-09 17:52:29 +0000

    madlib game

---
+++ Questionnaire.py
@@ -0,0 +1,69 @@
+Madlib =input ("Hi Jida! Welcome to this exciting game. Type 'YES' if you wish to proceed(Yes/No)")
+if Madlib == 'Yes':
+    print("Hurrraaayyy!!! Hope you enjoy the ride")
+if Madlib == 'No':
+    print("That's sad! Hopefully we can do this some other time")
+
+Day = input("How was work today?(Awesome/Bad)")
+if Day == 'Awesome':
+    print("That's because i remembered to say a prayer for you today")
+else:
+    print("Don't worry, the best is yet to come")
+    
+Gift = input("I hope you got me something on your way back from work?(Yes/No)")
+if Gift == 'Yes':
+    print("Awww..Please keep it for me,i would come for it very soon")
+else:
+    print("That's rather unfortunate :'(")
+
+Deborah = input("When last did you hear from Debbie?(Today/Yesterday/Days ago)")
+if Deborah == 'Today':
+    print("That's good to know")
+elif Deborah == 'Yesterday':
+    print("That's great!")
+else:
+    print("You should check up on her, she would like that")
+
+age = int(input("I hope you wouldn't mind answering some personal questions. What is your age?"))
+
+if age > 30:
+    print("I see you have a lot of experiences in life")
+elif age < 30:
+    print("I see you are still such a very young lady")
+else:
+    print("I can't wait to meet your mini version soon")
+
+exercise = input("Have you lost some weight?(Yes/No)")
+if exercise == 'No':
+    print("You can still get your body in shape for summer, all the best.")
+if exercise == 'Yes':
+    print("I love your dedication to your weightloss goals. keep it up!")
+Relationship = input("Are you in a relationship?(Yes/No)")
+if Relationship != 'No':
+    print("I would really love to meet the lucky guy soon")
+else:
+    print("That's also good. The best is yet to come")
+Feminist = input("Are you a feminist?(Yes?No)")
+if Feminist != 'No':
+    print("why do you think so many guys are not in support of feminism?(I can't tell/ They are still living in the past)")
+else:
+    print("That's awesome")
+Dance = input("Are you a better dancer than Debbie?(Yes/No)")
+if Dance == 'Yes':
+    print('Then you must be a really good dancer because Debbie can really dance')
+else:
+    print("I guess we have something in common :)")
+Cook = input("Who would you regard as the better cook?(Me/Debbie)")
+if Cook != 'Debbie':
+    print('Thumbs up')
+else:
+    print("That's great")
+End = input("Thank you for taking this ride with me,i wish it could have been longer.")
+Feedback = input("Hope you had fun?(Yes/No)")
+if Feedback == 'Yes':
+    print("Yaaay!!")
+else:
+    print("I can only get better at this, watch out for me. :)")
+    
+
+
