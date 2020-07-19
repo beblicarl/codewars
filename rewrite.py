7af548744f90971b6a0f4c0ece96bf9c1d05510d
Bebli Carl <beblicarl.cb@gmail.com>
2020-07-19 12:26:05 +0000

    4,6 atm code pins

---
+++ rewrite.py
@@ -0,0 +1,18 @@
+# Input is a string , output is a boolean
+# I will check the length of the string
+# If the length of the string is neither 4 or 6 return False
+# If the length is True check if the characters are numerical
+# If the characters are numerical return True
+# If the characters are not numerical return False
+
+
+def validate_pin(pin_code):
+    pin_length = len(pin_code) 
+    if pin_length != 4 and pin_length != 6:
+        return False
+    elif pin_code.isnumeric(): 
+        return True
+    else:
+        return False 
+
+print(validate_pin('0012'))
