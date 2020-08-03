b27c1465c55e083e84a1aca2ced83b9ee24eae10
Bebli Carl <beblicarl.cb@gmail.com>
2020-08-03 11:07:31 +0000

    updated second codewars attempt

--- codewars2.py
+++ codewars2.py
@@ -1,18 +1,31 @@
+#Given an array of integers your solution should find the smallest integer.
+
+#For example:
+
+    #Given [34, 15, 88, 2] your solution will return 2
+    #Given [34, -345, -1, 100] your solution will return -345
+
+#You can assume, for the purpose of this kata, that the supplied array will not be empty.
+
+
+
+
+
+
 #input is a list, output is an integer
 #Take the first number, compare with the second number and idenitfy the least number between them
 #Take the least number identified and compare to the third number and identify the new least number.
 #Continue this process until you exhaust the list.
 #Return the least number when you are done.
 
-numbers = []
 
 
-small_number = 0
-for smallest_number in numbers:
-    if smallest_number < small_number:
-        small_number = smallest_number
-    
+def find_smallest_int(arr):
+  small_number = arr[0]
+  for smallest_number in arr:
+      if smallest_number < small_number:
+          small_number = smallest_number
+  return small_number
 
-
-print(small_number)
+print(find_smallest_int([0, 1-2**64, 2**64]))
 
